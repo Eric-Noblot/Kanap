d399bc96424d2218816249d330455dd050a3a9c7
Eric Noblot <eric.noblot@gmail.com>
2023-03-14 14:09:46 +0100

    Ajout du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,15 +1,17 @@
 const products = await(await fetch("http://localhost:3000/api/products")).json();
 
-//textDisplay();
+
 
 
 // -------------------------------------------
 //FONCTION QUI AFFICHE LES ÉLÉMENTS DU PANIER
 // -------------------------------------------
-
+let totalArticles = 0;
 function textDisplay() {
+  
   //products.forEach(product => console.log(product.name));
   products.forEach(function (product) {
+    
     let article = document.querySelector("#cart__items").innerHTML +=
       `
  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
@@ -34,26 +36,24 @@ function textDisplay() {
     </div>
   </article>
   `
+
+  totalArticles = totalArticles + product.price
+
   })
+  console.log(totalArticles);
 }
 
-// --------------------------------
-// INPUTS ET FORMULAIRE
-// --------------------------------
+// ------------------------------------------------------------
+// FONCTION QUI VALIDE LES CHAMPS DE SAISIE DU FORMULAIRE
+// ------------------------------------------------------------
 
-const order = document.querySelector("#order");
-order.addEventListener("submit", function () {
-  alert("Go !");
-})
-
-ValidForm()
 function ValidForm(){
   let input = document.getElementById("firstName");
   input.addEventListener("keyup", function () {
-    let regex = /^[a-zA-Z-_éÉèÈêÊëËâÂäÄàÀôÔöÖ ]+$/;    //Utilier new Regex ?
+    let regex = /^[a-zA-Z-_éÉèÈêÊëËâÂäÄàÀôÔöÖ ]+$/;    //Utiliser new Regex ?
     if (input.value == "") {
       let error = document.querySelector("#firstNameErrorMsg");
-      error.textContent = "Vous devez saisir votre prénom !";
+      error.textContent = "Vous devez saisir un prénom !";
       error.style.display = "inline";
       error.style.color = "red";
     } else if (regex.test(input.value) === false) {
@@ -69,6 +69,33 @@ function ValidForm(){
   })
 }   // TESTER LE SWITCH !!!
 
+
+//------------------------------------------
+//FONCTION QUI FAIT LE TOTAL DES PRIX
+//------------------------------------------
+let tableau = [14, 25, 25, 30]
+const total = function(prices){
+  let sum = 0;
+  for (let price of prices){
+      sum = sum + price
+  }
+  console.log (sum);
+}
+total(tableau);
+
+// function totalPrice(...prixTotal) {
+//   let total = 0;
+//   for (let price of prixTotal) {
+//     total += price;
+//   }
+//   console.log (total);
+// }
+
+
+ValidForm()
+textDisplay();
+
+// VIEUX CODE
 
 // ValidForm(firstname);
 // function ValidForm(element){
--- front/js/product.js
+++ front/js/product.js
@@ -5,18 +5,22 @@
 const idProduct = window.location.search;
 const urlParams = new URLSearchParams(idProduct);
 const id = urlParams.get("id");
-console.log(id);
+//console.log(id);
+let color = document.getElementById("colors");
 
-const products = await(await fetch("http://localhost:3000/api/products/" + id)).json();
-displayProduct(products);
+
+const product = await(await fetch("http://localhost:3000/api/products/" + id)).json();
+
+
+displayProduct(product);
 
 // --------------------------------
 // FONCTION QUI AFFICHE LE PRODUIT
 // --------------------------------
 
-function displayProduct(product){
-    //console.log(product);
-    if (product != null){
+function displayProduct(product) {  
+
+    if (product != null) {
         let item__img = document.querySelector(".item__img");
         item__img.innerHTML += `<img src="${product.imageUrl}" alt="Photo d'un canapé">`;
 
@@ -25,21 +29,80 @@ function displayProduct(product){
 
         let description = document.getElementById("description");
         description.textContent = product.description;
+        
 
-       for (let i = 0; i < product.colors.length; i++){
-        let option = document.createElement("option");       // Boucle qui liste les différentes couleurs
-        option.textContent = product.colors[i];
-        let color = document.getElementById("colors");
-        color.appendChild(option);
+        for (let ColorChoice in product.colors) {
+            let option = document.createElement("option");       // Boucle qui liste les différentes couleurs
+            option.textContent = product.colors[ColorChoice];
+            color.appendChild(option);
         }
-    }   
-    else{
+    }
+    else {
         console.log("Erreur dans la récupération du produit");
-    }                             
+    }
+}
+
+//-------------------------------------------------------------
+// RÉCUPÉRATION DE LA VALUE SAISIE DANS L'INPUT COULEUR
+//-------------------------------------------------------------
+
+color.addEventListener("change", function(){
+    console.log(color.value)
+})
+
+//-------------------------------------------------------------
+// RÉCUPÉRATION DE LA VALUE SAISIE DANS L'INPUT QUANTITY
+//-------------------------------------------------------------
+
+let quantity = document.getElementById("quantity");
+quantity.addEventListener("keyup", function () {
+    //let quantityIput = parseInt(quantity.value)
+    if (isNumber(quantity.value)) {
+        console.log("c'est un chiffre");
+    }
+    else {
+        console.log("c'est une lettre");
+
+    }
+})
+
+// ------------------------------------------------------------
+//FONCTION QUI VÉRIFIE QUE LA QUANTITÉ SAISIE EST UN NOMBRE
+// ------------------------------------------------------------
+
+const isNumber = function (number) {
+    let regex = /^[0-9]+$/;             
+    if (regex.test(number)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
+// ------------------------------------------------------------
+// GESTION D'ÉVÈNEMENT DU BOUTON AJOUTER AU PANIER
+// ------------------------------------------------------------
 
+let buttonCart = document.getElementById("addToCart");
+buttonCart.addEventListener("click", function () {
+    if (isNumber(quantity.value) && quantity.value > 0) {
+        alert("Vous avez choisi de commander " + quantity.value + " article(s)");
+        quantity.value = 0;
+    }
+    else {
+        alert("Vous n'avez pas saisi la bonne quantité");
+        quantity.value = 0;
+    }
+})
+
+
+
+
+
+
+// VIEUX CODE
 
 //a href= "./product.html?id=${article._id}"
 // searchProduct();
@@ -51,3 +114,12 @@ function displayProduct(product){
 //         alert(error)
 //       });
 // }
+
+
+        //for (let i = 0; i < product.colors.length; i++){
+        //     let option = document.createElement("option");       // Boucle qui liste les différentes couleurs
+        //     option.textContent = product.colors[i];
+        //     let color = document.getElementById("colors");
+        //     color.appendChild(option);
+
+            //}
